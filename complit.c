6519bb51b1852d9bc8815e8b2d02f23b57b27557
Jenerth22Rodriguez <Jenert.rodriguez@geappliances.com>
2022-10-26 17:20:48 -0400

    Minor code review

--- complit.c
+++ complit.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
-#define MAXTITL 41
-#define MAXAUTL 31
+#define MAX_TITLE_LENGTH 41
+#define MAX_AUTHOR_LENGTH 31
 
 // exercise 14.11
+enum
+{
+    ExpensiveBookScore = 84,
+};
 
-struct book{
-    char title[MAXTITL];
-    char author[MAXAUTL];
+struct book
+{
+    char title[MAX_TITLE_LENGTH];
+    char author[MAX_AUTHOR_LENGTH]; // designated Initializer
     float value;
 };
 
 int main(void)
 {
+    struct book read =
+    {
+        .author = "Joe",
+        .title = "made some tittle",
+        .value = 3.99
+    };
     struct book readfirst;
     int score;
+
     printf("Enter test score: ");
     scanf("%d",&score);
 
-    if(score >= 84)
+    if(score >= ExpensiveBookScore)
     {
-        readfirst = (struct book){
-            "Crime and Punishment",
-            "Fyodorm Dostoyevsky",
-            11.25};
+
+        readfirst = read;
+    //     readfirst = (struct book){
+    //         "Crime and Punishment",
+    //         "Fyodorm Dostoyevsky",
+    //         11.25};
     }
     else {
         readfirst = (struct book){
