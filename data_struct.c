eee2a950e862d5626a63234ef188cf3cf4cd30ab
Jenerth22Rodriguez <Jenert.rodriguez@geappliances.com>
2022-11-09 15:36:57 -0500

    I just erased the comments

--- data_struct.c
+++ data_struct.c
@@ -28,11 +28,3 @@ printf("The model of the car 1 is : %s\n",Chevrolet.model);
 printf("The year of the car 1 is : %d\n",Chevrolet.years);
 return 0;
 }
-
-
-/*
-Working in a struct that is going to print the model, branch and year of the car
-I store two type of branch name in my struct for a simple example and then I print them
-in my main function. In this exersice I find out that char and string need to be in quation to
-print them, but the integer and number not need to be in quation to print them.
-*/
