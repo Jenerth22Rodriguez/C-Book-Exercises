832e32c2b2b8dcadf01f9c0ba65320fa3ed3e41f
Jenerth22Rodriguez <Jenert.rodriguez@geappliances.com>
2022-11-10 16:45:24 -0500

    Rename the variables

--- book_inventory.c
+++ book_inventory.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-
 #define LENGTH_TITLE (41)
 #define LENGTH_AUTHOR (31)
-
 char *s_gets(char *state_book, int number_of_book);
-
 struct book_price
 {
    char title[LENGTH_TITLE];
    char author[LENGTH_AUTHOR];
    float value_of_book;
 };
-
 int main(void)
 {
    struct book_price book;
@@ -25,22 +21,20 @@ int main(void)
    printf("%s by %s: $%.2f\n", book.title, book.author, book.value_of_book);
    printf("%s: \"%s\" ($%.2f)\n", book.author, book.title, book.value_of_book);
    printf("Done.\n");
-
    return 0;
 }
 
 char *s_gets(char *st, int length)
 {
-   char *return_book_value;
-   char *find_book;
-
-   return_book_value = fgets(st, length, stdin);
-   if(return_book_value)
+   char *return_value;
+   char *found_string;
+   return_value = fgets(st, length, stdin);
+   if(return_value)
    {
-      find_book = strchr(st, '\n');
-      if(find_book)
+      found_string = strchr(st, '\n');
+      if(found_string)
       {
-         *find_book = '\0';
+         *found_string = '\0';
       }
       else
       {
@@ -50,5 +44,5 @@ char *s_gets(char *st, int length)
          }
       }
    }
-   return return_book_value;
+   return return_value;
 }
