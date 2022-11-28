d0e06caf76d9f00ea49df9d2f55fc1d7540272e9
Jenerth22Rodriguez <Jenert.rodriguez@geappliances.com>
2022-11-28 12:14:19 -0500

    avoid having two checks nearly identical

--- global.c
+++ global.c
@@ -4,15 +4,12 @@ int main(void)
 {
    int units;
    printf("How many pounds to a firkin of butter?\n");
-   scanf("%u", &units);
+   scanf("%d", &units);
    while(units != 56)
    {
       printf("No luck, my friend. Try again.\n");
       scanf("%d", &units);
-      if(units == 56)
-      {
-         printf("You must have looked it up!\n");
-      }
    }
+   printf("You must have looked it up!\n");
    return 0;
 }
