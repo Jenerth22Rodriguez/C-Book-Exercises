8f194f3dcf6b1f5428b1d1d9e89b9eec39e9618a
Jenerth22Rodriguez <Jenert.rodriguez@geappliances.com>
2022-11-28 16:00:53 -0500

    Cleaning the code

--- invert.c
+++ invert.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 #include <limits.h>
 
-static char *convert_number_to_binary(int, char *);
-static void breakup_bits_into_groups(const char *);
-static int invert_number(int number, int bits);
+static char *convert_number_to_binary(int negative_number, char *ps)
+{
+   int index;
+   const static int size = CHAR_BIT * sizeof(int);
+   for(index = size - 1; index >= 0; index--, negative_number >>= 1)
+      ps[index] = (01 & negative_number) + '0';
+   ps[size] = '\0';
+   return ps;
+}
+
+static void breakup_bits_into_groups(const char *string)
+{
+   int index = 0;
+   while(string[index])
+   {
+      putchar(string[index]);
+      if(++index % 4 == 0 && string[index])
+         putchar(' ');
+   }
+}
+
+static int invert_number(int number, int bits)
+{
+   int mask = 0;
+   int bit_value = 1;
+   while(bits-- > 0)
+   {
+      mask |= bit_value;
+      bit_value <<= 1;
+   }
+   return number ^ mask;
+}
+
 
 int main(void)
 {
@@ -26,37 +56,4 @@ int main(void)
    puts("Bye!");
 
    return 0;
-}
-
-char *convert_number_to_binary(int negative_number, char *ps)
-{
-   int index;
-   const static int size = CHAR_BIT * sizeof(int);
-   for(index = size - 1; index >= 0; index--, negative_number >>= 1)
-      ps[index] = (01 & negative_number) + '0';
-   ps[size] = '\0';
-   return ps;
-}
-
-void breakup_bits_into_groups(const char *string)
-{
-   int index = 0;
-   while(string[index])
-   {
-      putchar(string[index]);
-      if(++index % 4 == 0 && string[index])
-         putchar(' ');
-   }
-}
-
-int invert_number(int number, int bits)
-{
-   int mask = 0;
-   int bit_value = 1;
-   while(bits-- > 0)
-   {
-      mask |= bit_value;
-      bit_value <<= 1;
-   }
-   return number ^ mask;
 }
