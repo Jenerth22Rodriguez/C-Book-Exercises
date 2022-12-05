17bcb09341a91ff594d174f8d3a5f6dd8ad820e4
Jenerth22Rodriguez <Jenert.rodriguez@geappliances.com>
2022-12-05 17:07:22 -0500

    formatting resolve

--- lesser.c
+++ lesser.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
-static int lesser(int name, int name2);
-
-int main(void)
-{
-int evil1, evil2;
-
-   printf("Enter a pair of integers (q to quit):\n");
-   while(scanf("%d %d", &evil1, &evil2) == 2)
-   {
-      printf("The lesser of %d and %d is %d.\n", evil1, evil2, lesser(evil1, evil2));
-      printf("Enter a pair of integers (q to quit):\n");
-   }
-   printf("Bye.\n");
-   return 0;
-}
-
-int lesser(int firstNumber, int secondNumber)
+static int lesser(int firstNumber, int secondNumber)
 {
    int evilNumber;
    if(firstNumber < secondNumber)
       evilNumber = firstNumber;
    else
-      evilNumber = secondNumber;
+      (evilNumber = secondNumber);
    return evilNumber;
+}
+
+int main(void)
+{
+   int evilNumber1, evilNumber2;
+
+   printf("Enter a pair of integers (q to quit):\n");
+   int numberOfIntegerScan = scanf("%d %d", &evilNumber1, &evilNumber2);
+
+   while(numberOfIntegerScan == 2)
+   {
+      printf("The lesser of %d and %d is %d.\n", evilNumber1, evilNumber2, lesser(evilNumber1, evilNumber2));
+      printf("Enter a pair of integers (q to quit):\n");
+      numberOfIntegerScan = scanf("%d %d", &evilNumber1, &evilNumber2);
+   }
+   printf("Bye.\n");
+   return 0;
 }
