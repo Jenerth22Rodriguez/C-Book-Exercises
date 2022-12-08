4a70c8663d2005f99c1bc4e31276d718a2674d24
Jenerth22Rodriguez <Jenert.rodriguez@geappliances.com>
2022-12-08 16:18:50 -0500

    Cleaning up my code

--- 2DArray.c
+++ 2DArray.c
@@ -1,66 +1,60 @@
 #include <stdio.h>
 
-#define ROWS 3
-#define COLUMNS 4
-
-static void sum_Rows(int ar[][COLUMNS], int rows);
-static void sum_columns(int[][COLUMNS], int);
-static int sum2dArray(int (*ar)[COLUMNS], int rows);
-
-void sum_Rows(int ar[][COLUMNS], int rows)
+enum
 {
-   int rowings;
-   int columns;
-   int totalSumRows;
-   for(rowings = 0; rowings < rows; rowings++)
+   Rows = 3,
+   Columns = 4,
+};
+
+static void SumRows(int array[][Columns])
+{
+   for(int rowings = 0; rowings < Rows; rowings++)
    {
-      totalSumRows = 0;
-      for(columns = 0; columns < COLUMNS; columns++)
+      int totalSumRows = 0;
+      for(int columns = 0; columns < Columns; columns++)
       {
-         totalSumRows += ar[rowings][columns];
+         totalSumRows += array[rowings][columns];
       }
       printf("row %d sum: %d\n\n", rowings, totalSumRows);
    }
 }
 
-void sum_columns(int ar[][COLUMNS], int rows)
+static void SumColumns(int array[][Columns])
 {
-   int rowings;
-   int columns;
-   int totalSumColumns;
-   for(columns = 0; columns < COLUMNS; columns++)
+   for(int columns = 0; columns < Columns; columns++)
    {
-      totalSumColumns = 0;
-      for(rowings = 0; rowings < rows; rowings++)
+      int totalSumColumns = 0;
+      for(int rowings = 0; rowings < Rows; rowings++)
       {
-         totalSumColumns += ar[rowings][columns];
+         totalSumColumns += array[rowings][columns];
       }
       printf("col %d: sum = %d\n\n", columns, totalSumColumns);
    }
 }
-int sum2dArray(int ar[][COLUMNS], int rows)
-{
-   int rowings;
-   int columns;
-   int totalSum2DArray = 0;
-   for(rowings = 0; rowings < rows; rowings++)
-      for(columns = 0; columns < COLUMNS; columns++)
-      {
-         totalSum2DArray += ar[rowings][columns];
-      }
 
+static int Sum2dArray(int array[][Columns])
+{
+   int totalSum2DArray = 0;
+
+   for(int rowings = 0; rowings < Rows; rowings++)
+   {
+      for(int columns = 0; columns < Columns; columns++)
+      {
+         totalSum2DArray += array[rowings][columns];
+      }
+   }
    return totalSum2DArray;
 }
 
 int main(void)
 {
-   int List[ROWS][COLUMNS] = {
+   int list[Rows][Columns] = {
       { 2, 4, 6, 8 },
       { 3, 5, 7, 9 },
       { 12, 10, 8, 6 }
    };
-   sum_Rows(List, ROWS);
-   sum_columns(List, ROWS);
-   printf("Sum of all elements = %d\n", sum2dArray(List, ROWS));
+   SumRows(list);
+   SumColumns(list);
+   printf("Sum of all elements = %d\n", Sum2dArray(list));
    return 0;
 }
