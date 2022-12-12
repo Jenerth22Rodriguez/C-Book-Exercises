21666b882a6a01a45217965aa4ad501821292860
Jenerth22Rodriguez <Jenert.rodriguez@geappliances.com>
2022-12-12 09:11:42 -0500

    Adding formatting

--- Arf.c
+++ Arf.c
@@ -2,36 +2,34 @@
 
 enum
 {
-   Size = 5,
+   SizeOfArray = 5,
 };
 
-static void ShowArray(const double arrayList[], int numberInsideArray)
+static void ShowArray(const double array[], int sizeOfArray)
 {
-   int index;
-   for(index = 0; index < numberInsideArray; index++)
+   for(int index = 0; index < sizeOfArray; index++)
    {
-      printf("%8.3f", arrayList[index]);
+      printf("%8.3f", array[index]);
       putchar('\n');
    }
 }
 
-static void MultArray(double ArrayList[], int NumberInsideArray, double mult)
+static void MultiplyElementsOfArrayByFactor(double array[], int sizeOfArray, double factor)
 {
-   int index;
-   for(index = 0; index < NumberInsideArray; index++)
+   for(int index = 0; index < sizeOfArray; index++)
    {
-      ArrayList[index] *= mult;
+      array[index] *= factor;
    }
 }
 
 int main(void)
 
 {
-   double dip[Size] = { 20.0, 17.66, 8.2, 15.3, 22.22 };
+   double array[SizeOfArray] = { 20.0, 17.66, 8.2, 15.3, 22.22 };
    printf("The original dip array:\n");
-   ShowArray(dip, Size);
-   MultArray(dip, Size, 2.5);
+   ShowArray(array, SizeOfArray);
+   MultiplyElementsOfArrayByFactor(array, SizeOfArray, 2.5);
    printf("The dip array after calling mult_array():\n");
-   ShowArray(dip, Size);
+   ShowArray(array, SizeOfArray);
    return 0;
 }
